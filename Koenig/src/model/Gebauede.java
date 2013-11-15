5c0d5710ba838c412e88d1dcbf222e93afea0bd3
Isarani <Isarani@gmx.de>
2013-11-15 19:34:27 +0100

    Gebäudepreis static. Getter dazu auch static

--- Koenig/src/model/Gebauede.java
+++ Koenig/src/model/Gebauede.java
@@ -3,11 +3,11 @@ package model;
 public class Gebauede {
 
 	private int kapazitaet;
-	private int preis;
+	private static int preis;
 	private String gebaudeArt;
 	
 	
-	public int getPreis(){
+	public static int getPreis(){
 		return preis;
 	}
 	
