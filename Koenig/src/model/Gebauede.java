d488cf82d265645039652e61ba1b767cbbc60cb6
admin <admin@E6530-NB20.uni-bremen.de>
2013-11-15 14:19:47 +0100

    Klassen gerüste gebaut

---
+++ Koenig/src/model/Gebauede.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Gebauede {
+
+	private int kapazitaet;
+	private int preis;
+	private String gebaudeArt;
+	
+	
+	public int getPreis(){
+		return preis;
+	}
+	
+	public String getGebaudeArt(){
+		return gebaudeArt;
+	}
+	
+	
+}
---
+++ Koenig/src/model/Muehle.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Muehle extends Gebauede {
+
+}
